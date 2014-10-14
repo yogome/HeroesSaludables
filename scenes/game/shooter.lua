1ea4f31d3adf34112c3a9192098e0381229a0ce4
Jonatan Santos <inetzsp@gmail.com>
2014-10-13 19:15:09 -0500

    Added main mechanic

--- scenes/game/shooter.lua
+++ scenes/game/shooter.lua
@@ -16,9 +16,91 @@ local dynamicObjects
 local analogX, analogY
 local analogCircleBegan
 local analogCircleMove
+local spaceObjects, objectDespawnX, objectSpawnX, objectDespawnY, objectSpawnY
+local spawnZoneWidth, spawnZoneHeight, halfSpawnZoneWidth, halfSpawnZoneHeight
+local backgroundGroup
 ----------------------------------------------- Constants
 local padding = 16
+local SIZE_BACKGROUND = 1024
+local OBJECTS_TOLERANCE_X = 100
+local OBJECTS_TOLERANCE_Y = 100
+local STARS_LAYER_DEPTH_RATIO = 0.08
+local STARS_PER_LAYER = 8
+local STARS_LAYERS = 6
 ----------------------------------------------- Functions
+--
+
+local function updateWorlds()
+	--ocal scrollX, scrollY = menu:getContentPosition()
+	--if not movingMenu then
+	--	selectedWorldIndex = math.round((-scrollX)/ SIZE_WORLD_ITEM)
+	--end
+	
+	if spaceObjects then
+		for index = 1, #spaceObjects do
+			local object = spaceObjects[index]
+			object.x = (object.xOffset + camera.scrollX * object.xVelocity) % spawnZoneWidth - halfSpawnZoneWidth
+			object.y = (object.yOffset + camera.scrollY * object.xVelocity) % spawnZoneHeight - halfSpawnZoneHeight
+		end
+	end
+end
+
+
+local function createBackground(sceneGroup)
+	backgroundGroup = display.newGroup()
+	
+	local dynamicScale = display.viewableContentWidth / SIZE_BACKGROUND
+    local backgroundContainer = display.newContainer(display.viewableContentWidth + 2, display.viewableContentHeight + 2)
+    backgroundContainer.x = display.contentCenterX
+    backgroundContainer.y = display.contentCenterY
+    sceneGroup:insert(backgroundContainer)
+    
+    local background = display.newImage("images/backgrounds/space.png", true)
+    background.xScale = dynamicScale
+    background.yScale = dynamicScale
+	--background.fill.effect = "filter.monotone"
+	--background.fill.effect.r, background.fill.effect.g, background.fill.effect.b = unpack(COLOR_BACKGROUND)
+    backgroundContainer:insert(background)
+	
+	local containerHalfWidth = backgroundContainer.width * 0.5
+	local containerHalfHeight = backgroundContainer.height * 0.5
+	
+	spaceObjects = {}
+	
+	objectDespawnX = containerHalfWidth + OBJECTS_TOLERANCE_X
+	objectSpawnX = -containerHalfWidth - OBJECTS_TOLERANCE_X
+	
+	objectDespawnY = containerHalfHeight + OBJECTS_TOLERANCE_Y
+	objectSpawnY = -containerHalfHeight - OBJECTS_TOLERANCE_Y
+	
+	spawnZoneWidth = -objectSpawnX + objectDespawnX
+	spawnZoneHeight = -objectSpawnY + objectDespawnY
+	halfSpawnZoneWidth = spawnZoneWidth * 0.5
+	halfSpawnZoneHeight = spawnZoneHeight * 0.5
+	
+	for layerIndex = 1, STARS_LAYERS do
+		local starLayer = display.newGroup()
+		backgroundContainer:insert(starLayer)
+		for starsIndex = 1, STARS_PER_LAYER do
+			local scale =  0.05 + layerIndex * 0.05
+			
+			local star = display.newCircle(500, 0, 20)
+			star.xOffset = math.random(objectSpawnX, objectDespawnX)
+			star.yOffset = math.random(objectSpawnY, objectDespawnY)
+			star.y = math.random(-containerHalfHeight, containerHalfHeight)
+			star.xScale = scale
+			star.yScale = scale
+			star.xVelocity = STARS_LAYER_DEPTH_RATIO * layerIndex
+			starLayer:insert(star)
+			
+			spaceObjects[#spaceObjects + 1] = star
+		end
+	end
+	
+	
+	backgroundGroup:insert(backgroundContainer)
+end
+
 local function onReleasedBack()
 	composer.gotoScene( "scenes.menus.map", { effect = "fade", time = 800, } )
 end 
@@ -36,7 +118,7 @@ local function testTouch(event)
 		analogCircleBegan.isVisible = true
 		analogCircleMove.isVisible = true
 	elseif "moved" == event.phase then
-		analogX = event.x - event.xStart
+		analogX = event.x - event.xStart 
 		analogY = event.y - event.yStart
 		analogCircleMove.x = event.x
 		analogCircleMove.y = event.y
@@ -49,27 +131,28 @@ local function testTouch(event)
 end
 
 local function createTestWalls()
-	local testRect1 = display.newRect(-500,0,40,1000)
+	
+	local testRect1 = display.newRect(-1000-25,0,50, 2000)
 	physics.addBody( testRect1, {density = 1.0, friction = 0.1, bounce = 0.5})
-	testRect1:setFillColor(1,1,0)
+	--testRect1:setFillColor(1,1,0)
 	testRect1.bodyType = "static"
 	camera:add(testRect1)
 	
-	local testRect2 = display.newRect(500,0,40,1000)
+	local testRect2 = display.newRect(1000+25, 0 ,50,2000)
 	physics.addBody( testRect2, {density = 1.0, friction = 0.1, bounce = 0.5})
-	testRect2:setFillColor(1,1,0)
+	--testRect2:setFillColor(1,1,0)
 	testRect2.bodyType = "static"
 	camera:add(testRect2)
 	
-	local testRect3 = display.newRect(0,1000,1800,40)
+	local testRect3 = display.newRect(0,-1000-25,2000,50)
 	physics.addBody( testRect3, {density = 1.0, friction = 0.1, bounce = 0.5})
-	testRect3:setFillColor(1,1,0)
+	--testRect3:setFillColor(1,1,0)
 	testRect3.bodyType = "static"
 	camera:add(testRect3)
 	
-	local testRect4 = display.newRect(0,-500,1000,40)
+	local testRect4 = display.newRect(0,1000+25,2000,50)
 	physics.addBody( testRect4, {density = 1.0, friction = 0.1, bounce = 0.5})
-	testRect4:setFillColor(1,1,0)
+	--testRect4:setFillColor(1,1,0)
 	testRect4.bodyType = "static"
 	camera:add(testRect4)
 	
@@ -127,10 +210,10 @@ end
 
 local function enterFrame()
 	local velocityX, velocityY = playerCharacter:getLinearVelocity()
-	sound.setPitch(0.8 + (math.abs(velocityX) + math.abs(velocityY)) * 0.0005)
+	--sound.setPitch(0.8 + (math.abs(velocityX) + math.abs(velocityY)) * 0.0005)
 	
 	if analogX and analogY then
-		playerCharacter:analog(analogX * 0.05, analogY * 0.05)
+		playerCharacter:analog(analogX * 0.01, analogY * 0.01)
 	end
 end
 
@@ -163,8 +246,8 @@ function scene:createGame()
 	dynamicObjects = {}
 	
 	createPlayerCharacter()
-	createEnemy()
-	createTestCubes()
+	--createEnemy()
+	--createTestCubes()
 	createTestWalls()
 	
 	spaceships.start()
@@ -193,10 +276,12 @@ end
 function scene:create(event)
 	local sceneGroup = self.view
 
+	createBackground(sceneGroup)
+
+	sceneGroup:insert(backgroundGroup)
 	camera = perspective.newCamera()
-	camera:setBounds(false)
+	camera:setBounds(-500, 500, -750, 750)
 	sceneGroup:insert(camera)
-	
 	buttonList.back.onRelease = onReleasedBack
 	buttonBack = widget.newButton(buttonList.back)
 	buttonBack.x = display.screenOriginX + 64 + padding
@@ -212,6 +297,7 @@ function scene:create(event)
 	analogCircleMove.alpha = 0.2
 	analogCircleMove.isVisible = false
 	sceneGroup:insert(analogCircleMove)
+	
 end
 
 function scene:destroy()
@@ -223,6 +309,8 @@ function scene:show( event )
     local phase = event.phase
 
     if ( phase == "will" ) then
+		
+		Runtime:addEventListener("enterFrame", updateWorlds)
 		self:createGame()
 		self.disableButtons()
 	elseif ( phase == "did" ) then
