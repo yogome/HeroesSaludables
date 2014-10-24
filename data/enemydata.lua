2dca25bb46f7a723308ce56689f275df5341bad5
Jonatan Santos <inetzsp@gmail.com>
2014-10-24 18:32:33 -0500

    Added Enemy data

---
+++ data/enemydata.lua
@@ -0,0 +1,16 @@
+local enemies = {
+	{
+		type = "canoner",
+		asset = "images/enemies/canon.png"
+	},
+	{	
+		type = "shooter",
+		asset = "images/enemies/shooter.png"
+	},
+	{
+		type = "follower",
+		asset = "images/enemies/follower.png"
+	}
+}
+
+return enemies
