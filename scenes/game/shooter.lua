6ffb4b500186601e2861bba7816769c5479c20d0
Jonatan Santos <inetzsp@gmail.com>
2014-10-24 18:34:14 -0500

    Updated main game
    Added game level loader

--- scenes/game/shooter.lua
+++ scenes/game/shooter.lua
@@ -1,6 +1,7 @@
 ----------------------------------------------- Shooter space game
 local composer = require( "composer" )
 local widget = require( "widget" )
+local settings = require("settings")
 local buttonList = require( "data.buttonlist" )
 local sound = require( "libs.helpers.sound" )
 local perspective = require( "libs.helpers.perspective" )
@@ -8,6 +9,8 @@ local robot = require( "libs.helpers.robot" )
 local spaceships = require( "entities.spaceships" )
 local physics = require( "physics" )
 local worldsData = require("data.worldsdata")
+local foodlist = require("data.foodlist")
+local enemy = require("entities.enemies")
 
 local scene = composer.newScene() 
 
@@ -26,12 +29,17 @@ local spawnZoneWidth, spawnZoneHeight, halfSpawnZoneWidth, halfSpawnZoneHeight
 -----------------------------------------------
 
 local backgroundGroup
+local foodBubbleGroup
+local enemies
+local isFruitSpawned, isVegetableSpawned, isProteinSpawned
+local totalCollectedFruits, totalCollectedVegetables, totalCollectedProteins
 -----------------------------------------------Vars used by level loader
 local levelBackground
 local levelSize
 local levelSpaceshipPosition
 local levelPlanets
 
+local debugText
 -----------------------------------------------Temporal vars
 local current_level = 1
 ----------------------------------------------- Constants
@@ -45,7 +53,160 @@ local STARS_LAYERS = 6
 
 local BOUNDARY_SIZE = 200
 ----------------------------------------------- Functions
---
+
+local function updateEnemies()
+	for indexEnemy = 1, #enemies do
+		enemies[indexEnemy]:patrol()
+	end
+	
+end
+
+local function showDebugInformation()
+	
+	debugText.text = string.format([[
+	ShipHasItem: %s
+	SpawnedFruit: %s
+	SpawnedVegetable: %s
+	SpawnedProtein: %s
+	Fruits: %d
+	Vegetables: %d
+	Proteins: %d
+	Enemies: %d
+	Player x: %d
+	Player y: %d]], 
+	tostring(playerCharacter.isCarringItem),
+	tostring(isFruitSpawned),
+	tostring(isVegetableSpawned),
+	tostring(isProteinSpawned),
+	totalCollectedFruits,
+	totalCollectedVegetables,
+	totalCollectedProteins,
+	#enemies,
+	playerCharacter.x,
+	playerCharacter.y)
+end
+
+local function randomFoodByType(foodType)
+	local bubbleSearchIndexes = {}
+	for indexBubble = 1, #foodBubbleGroup.bubbles do
+		local currentBubble = foodBubbleGroup.bubbles[indexBubble]
+		if currentBubble.type == foodType then
+			bubbleSearchIndexes[#bubbleSearchIndexes + 1] = indexBubble
+		end
+	end
+	return bubbleSearchIndexes[math.random(1, #bubbleSearchIndexes)]
+end
+
+local function spawnBubble(foodType, planet)
+	local foodBubbleIndex = randomFoodByType(foodType)
+	local foundBubble = foodBubbleGroup.bubbles[foodBubbleIndex]
+	
+	local showBubble = function()
+		foundBubble.x = planet.x
+		foundBubble.y = planet.y + planet.width * 0.5
+		foundBubble.alpha = 1
+		foundBubble.isVisible = true
+		foundBubble.isSpawned = true
+		
+		physics.addBody(foundBubble, "dynamic", {density = 0, friction = 0, bounce = 0.1, radius = 30})
+		foundBubble.gravityScale = 0
+		foundBubble:applyLinearImpulse( math.random(-5,5)/100, math.random(-5,5)/100, foundBubble.x, foundBubble.y)
+	end
+	
+	timer.performWithDelay(100, showBubble)
+end
+
+local function grabFruit(fruit)
+	--transition.to(fruit, { x = playerCharacter.x - 62, y = playerCharacter.y + 5 , onComplete = function()
+			physics.removeBody(fruit)
+			fruit.x = -62
+			fruit.y = 5
+			playerCharacter:insert(fruit)
+	--end})
+end
+
+local function preCollision(self, event)
+	if not playerCharacter.isCarringItem then
+		if event.object1.name == "player" and event.object2.type == "fruit" or event.object2.type == "protein" or event.object2.type == "vegetable" then
+			playerCharacter.isCarringItem = true
+			playerCharacter.item = event.object2
+			event.object2.isSensor = true
+			local function fruitLister()
+				grabFruit(event.object2)
+			end
+			timer.performWithDelay(100, fruitLister)
+		end
+	end
+	
+end
+
+local function hideBubble()
+	playerCharacter.item.isVisible = false
+	playerCharacter.isCarringItem = false
+	if playerCharacter.item.type == "fruit" then
+		isFruitSpawned = false
+	elseif playerCharacter.item.type == "vegetable" then
+		isVegetableSpawned = false
+	elseif playerCharacter.item.type == "protein" then
+		isProteinSpawned = false
+	end
+	--playerCharacter.item = nil
+end
+
+local function updateScore(type)
+	if type == "fruit" then
+		totalCollectedFruits = totalCollectedFruits + 1
+	elseif type == "vegetable" then
+		totalCollectedVegetables = totalCollectedVegetables + 1
+	elseif type == "protein" then
+		totalCollectedProteins = totalCollectedProteins + 1
+	end
+end
+	
+local function localCollision(self, event)
+	event.object1.name = event.object1.name or "unknown"
+	event.object2.name = event.object2.name or "unknown"
+	
+	if ( event.phase == "began" ) then
+
+		print("I am " .. event.object2.name) 
+			--Collisions on planet range
+		if event.object1.name == "fruits" and isFruitSpawned == false then
+			spawnBubble("fruit", event.object1)
+			isFruitSpawned = true
+		elseif event.object1.name == "proteins" and isProteinSpawned == false then
+			spawnBubble("protein", event.object1)
+			isProteinSpawned = true
+		elseif event.object1.name == "vegetables" and isVegetableSpawned == false then
+			spawnBubble("vegetable", event.object1)
+			isVegetableSpawned = true
+		elseif event.object1.name == "earth" and playerCharacter.isCarringItem then
+		
+			local function bubbleToEarth()
+				foodBubbleGroup:insert(playerCharacter.item)
+				playerCharacter.item.x = playerCharacter.x
+				playerCharacter.item.y = playerCharacter.y
+				transition.to(playerCharacter.item, {transition = easing.outBounce, xScale = 1, yScale = 1, time = 500, onComplete = function()
+					transition.to(playerCharacter.item, {alpha = 0, xScale = 0.5, yScale = 0.5, transition = easing.outCubic, time = 1000, x = event.object1.x, y = event.object1.y, onComplete = function()
+						hideBubble()
+					end})
+				end})
+			end
+			
+			updateScore(playerCharacter.item.type)
+			timer.performWithDelay(100, bubbleToEarth)
+		else
+			print("collided with: " .. event.object1.name)
+		end
+		
+
+    elseif ( event.phase == "ended" ) then
+        if(event.object1.name == "fruits" or event.object1.name == "proteins" or event.object1.name == "vegetables") then
+			print("Outside range with: " .. event.object1.name)
+		end
+    end
+end
+
 
 local function updateParallax()
 	if spaceObjects then
@@ -151,7 +312,7 @@ local function testTouch(event)
 	end	
 end
 
-local function createrBorders(level, world)
+local function createBorders(level, world)
 	
 	local levelData = worldsData[level][world]
 	local levelWidth = levelData.levelWidth
@@ -199,42 +360,6 @@ local function createrBorders(level, world)
 	addDynamicObject(topWall)
 end
 
-local function createTestCubes()
-	local testRect1 = display.newRect(-300,0,300,100)
-	physics.addBody( testRect1, {density = 1.0, friction = 0.3, bounce = 0.2})
-	testRect1:setFillColor(1,0,0)
-	testRect1.gravityScale = 0
-	camera:add(testRect1)
-	
-	addDynamicObject(testRect1)
-	
-	for indexA = 1, 8 do
-		for indexB = 1, 5 do
-			local testRect = display.newRect(-300 + 50 * indexA, -400 + 50 * indexB,40,40)
-			physics.addBody( testRect, {density = 1.0, friction = 0.3, bounce = 0.2})
-			testRect:setFillColor(1,0.5,0)
-			testRect.gravityScale = 0
-			camera:add(testRect)
-			
-			addDynamicObject(testRect)
-		end
-	end
-end
-
-local function createEnemy()
-	for index = 1, 4 do
-		local shipData = {
-
-		}
-		local enemy = spaceships.new(shipData)
-		enemy.y = 200
-		enemy.x = -200 + 100 * index
-		camera:add(enemy)
-
-		addDynamicObject(enemy)
-	end
-end
-
 local function setUpCamera()
 	
 	local levelData = worldsData[1][1]
@@ -246,16 +371,51 @@ local function setUpCamera()
 	camera:setBounds(false)
 end
 
-local function createPlayerCharacter()
-	local shipData = {
+local function createPlayerCharacter(world, level)
+	shipPosition = worldsData[world][level].ship.position
 	
-	}
 	playerCharacter = spaceships.new(shipData)
-	playerCharacter.x = -1500
-	playerCharacter.y = -700
+	playerCharacter.isCarringItem = false
+	playerCharacter.name = "player"
+	playerCharacter.x = shipPosition.x
+	playerCharacter.y = shipPosition.y
 	camera:add(playerCharacter)
 	
 	addDynamicObject(playerCharacter)
+end
+
+local function createBubble(type)
+	
+	local bubble = display.newImage("images/food/bubble.png")
+	local food = display.newImage(type.asset)
+	local bubbleGroup = display.newGroup()
+
+	bubbleGroup.name = type.name
+	bubbleGroup.type = type.type
+	bubbleGroup.isSpawned = false
+	bubbleGroup.isVisible = false
+	bubbleGroup:scale(0.5, 0.5)
+
+	bubbleGroup:insert(bubble)
+	bubbleGroup:insert(food)
+	
+	return bubbleGroup
+end
+
+local function createFoodBubbles()
+	foodBubbleGroup = display.newGroup()
+	foodBubbleGroup.bubbles = {}
+	local x = 100
+	for foodIndex = 1, #foodlist do
+		local currentFood = foodlist[foodIndex]
+		
+		local newBubble = createBubble(currentFood)
+		
+		foodBubbleGroup.bubbles[foodIndex] = newBubble
+
+		foodBubbleGroup:insert(newBubble)
+		camera:add(foodBubbleGroup)
+	end
 end
 
 local function enterFrame()
@@ -268,10 +428,13 @@ end
 local function loadEarth(world, level)
 	local earthData = worldsData[world][level].earth
 	local earth = display.newImage(earthData.asset)
-	
+	earth.xScale = earthData.scaleFactor
+	earth.yScale = earthData.scaleFactor
 	earth.x = earthData.position.x
 	earth.y = earthData.position.y
+	earth.name = earthData.name
 	
+	physics.addBody(earth, "static", {radius = earth.width * earth.xScale, isSensor = true})
 	camera:add(earth)
 end
 
@@ -282,6 +445,9 @@ local function loadPlanets(world, level)
 	for planetIndex = 1, #planetsData do
 		local currentPlanet = planetsData[planetIndex]
 		local planet = display.newImage(currentPlanet.asset)
+		physics.addBody(planet, "static", {density = 1, friction = 1, bounce = 1, radius = 200, isSensor = true})
+		planet.name = currentPlanet.name
+		
 		planet.x = currentPlanet.position.x
 		planet.y = currentPlanet.position.y
 		planets[planetIndex] = planet
@@ -299,7 +465,7 @@ local function loadAsteroids(world, level)
 		local y1 = currentAsteroidLine.lineStart.y
 		local y2 = currentAsteroidLine.lineEnd.y
 		
-		local asteroidsPerLine = math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2 )*(y1 - y2 ))/60
+		local asteroidsPerLine = math.ceil(math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2 )*(y1 - y2 ))/60)
 		local asteroidStepX = math.abs(x1 - x2) / asteroidsPerLine
 		local asteroidStepY = math.abs(y1 - y2)/ asteroidsPerLine
 		local asteroidStartX = x1
@@ -312,12 +478,23 @@ local function loadAsteroids(world, level)
 			asteroid.x = asteroidStartX
 			asteroid.y = asteroidStartY
 			
-			asteroidStartX = asteroidStartX + asteroidStepX
-			asteroidStartY = asteroidStartY - asteroidStepY
+			if x2 > x1 then
+				asteroidStartX = asteroidStartX + asteroidStepX
+			else
+				asteroidStartX = asteroidStartX - asteroidStepX
+			end
 			
-			physics.addBody(asteroid, {density = 1000, friction = 10, bounce = 0.5})
+			if y2 > y1 then
+				asteroidStartY = asteroidStartY + asteroidStepY
+			else
+				asteroidStartY = asteroidStartY - asteroidStepY
+			end
+			
+			
+			physics.addBody(asteroid, {density = 1000, friction = 10, bounce = 0.5, radius = asteroid.width * 0.5})
 			asteroid.gravityScale = 0
 			asteroid.bodyType = "static"
+			asteroid.name = "asteroid"
 			camera:add(asteroid)
 		end
 		
@@ -325,10 +502,71 @@ local function loadAsteroids(world, level)
 	
 end
 
+local function loadObjetives()
+	
+end
+
+local function loadEnemies(world, level)
+	local enemyData = worldsData[world][level].enemies
+	for indexEnemy = 1, #enemyData do
+		local currentEnemy = enemyData[indexEnemy]
+		local enemyObject = enemy.newEnemy(currentEnemy.type, currentEnemy.speed, currentEnemy.radius, currentEnemy.position.pathStart, currentEnemy.position.pathEnd)
+		enemyObject.group.type = "enemy"
+		enemyObject.group.name = enemyData[indexEnemy].type
+		enemies[indexEnemy] = enemyObject
+		camera:add(enemyObject.group)
+	end
+end
+
 local function loadLevel()
-	loadEarth(1,1)
-	loadPlanets(1, current_level)
-	loadAsteroids(1,1)
+	local level = 2
+	local world = 1
+	loadObjetives(world, level)
+	loadEarth(world, level)
+	loadPlanets(world, level)
+	loadAsteroids(world, level)
+	loadEnemies(world, level)
+end
+
+local function createGUI()
+	local userGUI = display.newGroup()
+	userGUI.x = display.screenOriginX
+	userGUI.y = display.screenOriginY
+	
+	local container =  display.newRoundedRect( 100, 300, 150, 250, 30)
+	container.strokeWidth = 3
+	container:setFillColor( 0.5, 0.5, 0.5, 0.5 )
+	container:setStrokeColor( 0.5,0,0 )
+	userGUI:insert(container)
+	
+	local fruitGroup = display.newGroup()
+	local fruitIcon = display.newImage("images/food/strawberry.png")
+	fruitIcon:scale(0.5,0.5)
+	fruitIcon.x = (container.x - container.width * 0.5) + container.width * 0.2
+	fruitIcon.y = container.y - (container.height * 0.5) + container.height * 0.20
+	
+	
+	local vegetableIcon = display.newImage("images/food/carrot.png")
+	vegetableIcon:scale(0.5,0.5)
+	vegetableIcon.x = (container.x - container.width * 0.5) + container.width * 0.2
+	vegetableIcon.y = fruitIcon.y + fruitIcon.height * 0.5
+	local proteinIcon= display.newImage("images/food/meat.png")
+	proteinIcon:scale(0.5,0.5)
+	proteinIcon.x = (container.x - container.width * 0.5) + container.width * 0.2
+	proteinIcon.y = vegetableIcon.y + vegetableIcon.height * 0.5
+	
+	userGUI:insert(fruitIcon)
+	userGUI:insert(vegetableIcon)
+	userGUI:insert(proteinIcon)
+	
+end
+
+local function resetData()
+	isFruitSpawned, isVegetableSpawned, isProteinSpawned = false, false, false
+	
+	totalCollectedFruits = 0
+	totalCollectedVegetables = 0
+	totalCollectedProteins = 0
 end
 ----------------------------------------------- Class functions 
 function scene.backAction()
@@ -346,8 +584,6 @@ end
 
 function scene:createGame()
 	system.activate("multitouch")
-	Runtime:addEventListener("touch", testTouch)
-	Runtime:addEventListener("enterFrame", enterFrame)
 	
 	physics.setPositionIterations(1)
 	physics.setVelocityIterations(1)
@@ -356,17 +592,26 @@ function scene:createGame()
 	physics.setGravity(0, 10)
 	
 	dynamicObjects = {}
+	enemies = {}
 	
 	loadLevel()
 	
-	createPlayerCharacter()
+	createPlayerCharacter(1,2)
 	--createEnemy()
-	--createTestCubes()
-	createrBorders(1, 1)
+	createBorders(1, 2)
+	createFoodBubbles()
 	
 	spaceships.start()
 	camera:setFocus(playerCharacter)
 	camera:start()
+	
+	playerCharacter.collision = localCollision
+	playerCharacter.preCollision = preCollision
+	
+	Runtime:addEventListener("touch", testTouch)
+	Runtime:addEventListener("enterFrame", enterFrame)
+	Runtime:addEventListener("collision", playerCharacter)
+	Runtime:addEventListener("preCollision", playerCharacter)
 end
 
 function scene:destroyGame()
@@ -390,6 +635,9 @@ end
 function scene:create(event)
 	local sceneGroup = self.view
 
+
+	createGUI(sceneGroup)
+	
 	createBackground(sceneGroup)
 	sceneGroup:insert(backgroundGroup)
 	
@@ -412,6 +660,9 @@ function scene:create(event)
 	analogCircleMove.isVisible = false
 	sceneGroup:insert(analogCircleMove)
 	
+	debugText = display.newText("", display.contentWidth - 100, display.screenOriginY + 100, settings.fontName, 15)
+	
+	
 end
 
 function scene:destroy()
@@ -424,10 +675,14 @@ function scene:show( event )
 
     if ( phase == "will" ) then
 		
-		Runtime:addEventListener("enterFrame", updateParallax)
+		resetData()
 		self:createGame()
 		self.disableButtons()
 	elseif ( phase == "did" ) then
+		
+		Runtime:addEventListener("enterFrame", updateParallax)
+		Runtime:addEventListener("enterFrame", updateEnemies)
+		Runtime:addEventListener("enterFrame", showDebugInformation)
 		self.enableButtons()
 	end
 end
