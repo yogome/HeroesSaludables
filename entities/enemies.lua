13ec4fb77e3ba586547e7cd8c01b0bdedd7a7246
Jonatan Santos <inetzsp@gmail.com>
2014-10-31 11:07:44 -0600

    Updated enemy entities

--- entities/enemies.lua
+++ entities/enemies.lua
@@ -2,7 +2,6 @@
 local extramath = require( "libs.helpers.extramath" ) 
 local physics = require( "physics" )
 local enemydata = require("data.enemydata")
-local physics = require("physics")
 
 local enemyEntity = {}
 ---------------------------------------------- Variables
@@ -18,49 +17,140 @@ local mathAbs = math.abs
 function enemyEntity.newEnemy(enemyType, speed, radius, pathStart, pathEnd)
 	local enemy = {}
 	enemy.radius = radius or 250
-	enemy.group = display.newGroup()
 	enemy.pathStart = pathStart or {x = 0,y = 0}
 	enemy.pathEnd = pathEnd or {x = 0, y = 0}
 	enemy.shipSpeed = speed
-	enemy.isStoped = false
 	
 	enemy.lengthX = enemy.pathEnd.x - enemy.pathStart.x
 	enemy.lengthY = enemy.pathEnd.y - enemy.pathStart.y
 	enemy.speedX = enemy.lengthX / enemy.shipSpeed
 	enemy.speedY = enemy.lengthY / enemy.shipSpeed
 	
+	enemy.group = display.newGroup()
+	enemy.group.isStoped = false
+	enemy.group.isPlayerOnRange = false
+	enemy.group.hasToReturn = false
+	enemy.group.canFollowObject = false
+	enemy.group.objectToFollow = nil
+	enemy.group.flipSide = "right"
+	enemy.group.returnX = 0
+	enemy.group.returnY = 0
+	enemy.group.returnSpeedX = 0
+	enemy.group.returnSpeedY = 0
+	enemy.group.calculatedDistance = false
+	
+	function enemy.group:flip(side)
+		if side == "right" then
+			if self.flipSide ~= "right" then
+				self.flipSide = "right"
+				self.xScale = 1
+			end
+		elseif side == "left" then
+			if self.flipSide ~= "left" then
+				self.flipSide = "left"
+				self.xScale = -1
+			end
+		end
+	end
+	
 	function enemy:setPosition(position)
 		if self.pathStart.x > self.pathEnd.x then
-			self.group.xScale = -1
+			self.group:flip("left")
 		end
+		
 		self.group.x = position.x
 		self.group.y = position.y
 	end
 	
-	function enemy:stop()
+	function enemy.group:stop()
 		self.isStoped = true
 	end
 	
-	function enemy:move()
+	function enemy.group:move()
 		self.isStoped = false
+	end
+	
+	function enemy.group:shoot()
+		
+	end
+	
+	function enemy:follow()
+		local stepX = self.group.objectToFollow.x - self.group.x
+		local stepY = self.group.objectToFollow.y - self.group.y
+		
+		if stepX > 0 then
+			self.group:flip("right")
+		else
+			self.group:flip("left")
+		end
+		
+		self.group.x = self.group.x + (stepX  * 0.006)
+		self.group.y = self.group.y + (stepY  * 0.006)
+	end
+	
+	function enemy:returnToPatrol()
+		--local distanceFromStart = math.sqrt((enemy.pathEnd.x - enemy.pathStart.x)*(enemy.pathEnd.x - enemy.pathStart.x) + (enemy.pathEnd.y - enemy.pathStart.y)*(enemy.pathEnd.y - enemy.pathStart.y))
+		self.group.calculatedDistance = false
+		if not self.group.calculatedDistance then
+			
+			self.group.returnSpeedX = mathAbs(self.group.x - self.group.returnX) * 0.005
+			self.group.returnSpeedY = mathAbs(self.group.y - self.group.returnY) * 0.005
+			
+			self.group.calculatedDistance = true
+		end
+		
+		if self.group.x > self.pathStart.x then
+			self.group.returnSpeedX = self.group.returnSpeedX * -1
+			self.group:flip("left")
+		else
+			self.group:flip("right")
+		end
+		
+		if self.group.y > self.pathStart.y then
+			self.group.returnSpeedY = self.group.returnSpeedY * -1
+		end
+		
+		self.group.x = self.group.x + self.group.returnSpeedX
+		self.group.y = self.group.y + self.group.returnSpeedY
+		
+	end
+	
+	function enemy.group:setReturnPoint()
+		if enemy.pathStart.x > enemy.pathEnd.x then
+			self.returnX = (enemy.pathStart.x - enemy.pathEnd.x)/2	
+		else
+			self.returnX = (enemy.pathStart.x + enemy.pathEnd.x)/2
+		end
+		
+		if enemy.pathStart.y > enemy.pathEnd.x then
+			self.returnY = (enemy.pathStart.y - enemy.pathEnd.y)/2
+		else
+			self.returnY = (enemy.pathStart.y + enemy.pathEnd.y)/2
+		end
+		
+	end
+	
+	function enemy.group:setFollowing(onrange, object)
+		self.isPlayerOnRange = onrange
+		self.objectToFollow = object
 	end
 	
 	function enemy:patrol()
 	
-		if not self.isStoped then
+		--if not self.group.isStoped and not self.group.isPlayerOnRange and not self.group.hasToReturn then
 			if self.pathEnd.x > self.pathStart.x then
 				if self.group.x < self.pathStart.x then
 					self.group.x = self.pathStart.x
 					self.group.y = self.pathStart.y
 					self.speedX = self.speedX * -1
-					self.group.xScale = self.group.xScale * -1
+					self.group:flip("right")
 				end
 
 				if self.group.x > self.pathEnd.x then
 					self.group.x = self.pathEnd.x
 					self.group.y = self.pathEnd.y
 					self.speedX = self.speedX * -1
-					self.group.xScale = self.group.xScale * -1
+					self.group:flip("left")
 				end
 			else
 
@@ -68,14 +158,14 @@ function enemyEntity.newEnemy(enemyType, speed, radius, pathStart, pathEnd)
 					self.group.x = self.pathStart.x
 					self.group.y = self.pathStart.y
 					self.speedX = self.speedX * -1
-					self.group.xScale = self.group.xScale * -1
+					self.group:flip("left")
 				end
 
 				if self.group.x < self.pathEnd.x then
 					self.group.x = self.pathEnd.x
 					self.group.y = self.pathEnd.y
 					self.speedX = self.speedX * -1
-					self.group.xScale = self.group.xScale * -1
+					self.group:flip("right")
 				end
 			end
 
@@ -107,7 +197,7 @@ function enemyEntity.newEnemy(enemyType, speed, radius, pathStart, pathEnd)
 
 			self.group.x = self.group.x + self.speedX
 			self.group.y = self.group.y + self.speedY
-		end
+		--end
 	end
 	
 	local function getEnemyTypes()
@@ -165,3 +255,4 @@ end
 
 
 return enemyEntity
+
