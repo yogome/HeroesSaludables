8ce0b5216700a8f40efd4d1e0f53b80cbc19bbd6
Jonatan Santos <inetzsp@gmail.com>
2014-10-31 19:28:27 -0600

    Updated buttons

--- data/buttonlist.lua
+++ data/buttonlist.lua
@@ -7,6 +7,11 @@ end
 ----------------------------------------------- Data
 local buttonlist = {
 	back = { width = 128, height = 128, defaultFile = "images/buttons/back_01.png", overFile = "images/buttons/back_02.png", onPress = playSound},
+	play = { width = 128, height = 128, defaultFile = "images/buttons/play_01.png", overFile = "images/buttons/play_02.png", onPress = playSound},
+	ok = { width = 128, height = 128, defaultFile = "images/buttons/bnt_ok.png", overFile = "images/buttons/bnt_ok.png", onPress = playSound},
+	settings = { width = 128, height = 128, defaultFile = "images/buttons/bnt_settings.png", overFile = "images/buttons/bnt_settings.png", onPress = playSound},
+	next = { width = 128, height = 112, defaultFile = "images/buttons/adelante.png", overFile = "images/buttons/adelante_02.png", onPress = playSound},
+	previous = { width = 128, height = 112, defaultFile = "images/buttons/atras.png", overFile = "images/buttons/atras_02.png", onPress = playSound},
 }
 
 return buttonlist
