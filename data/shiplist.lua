811726ba57d07f9924bdb2477d8af9097bcd7fde
Jonatan Santos <inetzsp@gmail.com>
2014-11-03 12:57:50 -0600

    Created shiplist

---
+++ data/shiplist.lua
@@ -0,0 +1,7 @@
+local shiplist = {
+	[1] = {spritesheet = "images/ships/ship1_a.png"},
+	[2] = {spritesheet = "images/ships/ship2_a.png"},
+	[3] = {spritesheet = "images/ships/ship3_a.png"},
+}
+
+return shiplist
