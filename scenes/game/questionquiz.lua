9048460e7e837779449a389499ee97eaca548d99
Jonatan Santos <inetzsp@gmail.com>
2014-11-04 17:13:45 -0600

    Fixed question answers

--- scenes/game/questionquiz.lua
+++ scenes/game/questionquiz.lua
@@ -40,7 +40,7 @@ local mathRandom = math.random
 local questions = {
 	[1] = {text = "¿Dónde puedes encontrar etiquetas nutrimentales?", answerid = 1, answers = {[1] = "En LA PARTE DE ATRAS todas las bebidas y alimentos empaquetados.", [2] = "En la tiendita.", [3] = "Llamando por teléfono al numero de emergencia.", [4] = "No se encuentra en ningún lugar.",}}, 
 	[2] = {text = "¿Qué es un envase?", answerid = 2, answers = {[1] = "Una juguete que sale en los dulces.", [2] = "La bolsita o el paquetito donde viene el producto o alimento.", [3] = "Ninguna de las opciones.", [4] = "Un alimento que se vende en la calle.",}},
-	[3] = {text = "¿Qué es una porción?", answerid = 3, answers = {[1] = "Es una parte, pedazo o una fraccion del producto total.", [2] = "Un producto entero", [3] = "Un pedazo de envoltura", [4] = "Es exactamente 20 gramos",}},
+	[3] = {text = "¿Qué es una porción?", answerid = 1, answers = {[1] = "Es una parte, pedazo o una fraccion del producto total.", [2] = "Un producto entero", [3] = "Un pedazo de envoltura", [4] = "Es exactamente 20 gramos",}},
 	--[4] = {text = "¿Pregunta 4(D)?", answerid = 4, answers = {[1] = "Respuesta A", [2] = "Respuesta B", [3] = "Respuesta C", [4] = "Respuesta D",}},
 	--[5] = {text = "¿Pregunta 5(A)?", answerid = 1, answers = {[1] = "Respuesta A", [2] = "Respuesta B", [3] = "Respuesta C", [4] = "Respuesta D",}},
 }
