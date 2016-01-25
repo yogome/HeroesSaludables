46ff39eebd85be90d48a4bbfd912670ad63f380a
SundaEoBit <inetzsp@gmail.com>
2016-01-25 12:24:16 -0600

    Removed plugins

--- settings.lua
+++ settings.lua
@@ -9,17 +9,7 @@ local settings = {
 	statusBar = display.HiddenStatusBar,
 	gameName = "HeroesSaludables",
 	gameVersion = "0.9",
-	pushWooshRegisterHostname = "https://cp.pushwoosh.com/json/1.3/registerDevice",
-	server = {
-		hostname = "https://heroesofknowledge.appspot.com",
-		--hostname = "http://192.168.15.69:8888",
-		--hostname = "http://localhost:8888",
-		contentType = "application/json",
-		appID = "NOID",
-		restKey = "NOKEY",
-	},
-	iOSAppId = 0000000,
-	bundle = "com.yogome.HeroesSaludables",
+	bundle = "com.yogome.profeco.HeroesSaludables",
 	languagesDataPath = "data/languages/",
 }
 
